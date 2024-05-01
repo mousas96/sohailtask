3202a0ccad4ddb3fdbc713008e428a7d6cc70348
mousas96 <103472279+mousas96@users.noreply.github.com>
2024-05-01 10:39:42 +0400

    Update task_tracker.py

--- task_tracker.py
+++ task_tracker.py
@@ -65,7 +65,7 @@ st.set_page_config(layout="wide")
 st.markdown(
     """
     <div style="text-align: center;">
-        <img src="sohailtask/sohail.png" alt="Logo" width="300">
+        <img src="https://app.box.com/s/g1ttcyk6kmaq7a7cicptemjsjwcnfjcg" alt="Logo" width="300">
     </div>
     """, unsafe_allow_html=True
 )
